78a919c1f834489367e1499fb71147e75047a079
RileyRIchards <richardsr3@spu.edu>
2020-04-06 21:20:10 -0700

    basic start

--- Path-Finding.py
+++ Path-Finding.py
@@ -1,3 +1,11 @@
 
 import pygame
 
+class box:
+    def __init__(self):
+
+rows, columns = (25, 25)
+
+#creates 2d array which is going to be used for the grid
+grid = [[0 for i in range(columns)] for j in range(rows)]
+
