89e08f2bdcce34783ee141086f9632d8fa2dd793
RileyRIchards <richardsr3@spu.edu>
2020-04-07 11:49:10 -0700

    init values

--- Path-Finding.py
+++ Path-Finding.py
@@ -3,8 +3,15 @@ import pygame
 pygame.init()
 
 
-class box:
+class Node:
     def __init__(self):
+        self.Gcost = 0  # distance from starting Node
+        self.Hcost = 0  # distance from end node
+        self.Fcost = 0  # Fcost = Gcost + Hcost
+        self.parentNode = None
+        self.neighbour = []
+        self.open = False
+        self.closed = False
 
 rows, columns = (25, 25) # sets the size of the grid
 
@@ -14,4 +21,4 @@ grid = [[0 for i in range(columns)] for j in range(rows)]
 # makes each element of the area equal to box
 for i in range(columns):
     for j in range(rows):
-        grid[i][j] = box
+        grid[i][j] = Node
