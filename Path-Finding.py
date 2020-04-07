ec08077e222ae679ff79825949b3862aedd110f6
RileyRIchards <richardsr3@spu.edu>
2020-04-06 21:27:16 -0700

    start day 1

--- Path-Finding.py
+++ Path-Finding.py
@@ -1,11 +1,17 @@
 
 import pygame
+pygame.init()
+
 
 class box:
     def __init__(self):
 
-rows, columns = (25, 25)
+rows, columns = (25, 25) # sets the size of the grid
 
-#creates 2d array which is going to be used for the grid
+# creates 2d array which is going to be used for the grid
 grid = [[0 for i in range(columns)] for j in range(rows)]
 
+# makes each element of the area equal to box
+for i in range(columns):
+    for j in range(rows):
+        grid[i][j] = box
