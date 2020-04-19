acd5f7bc09779b8eceaca1f04f2387bce417517d
Jordan Mielbrecht <mielbrechtj@spu.edu>
2020-04-19 00:59:02 -0700

    Interactive map-building FINISHED
    + Enables wall-building
    + Enables custom start/end point making

--- maze-draw.py
+++ maze-draw.py
@@ -1,12 +1,12 @@
-import pygame, random, numpy as np, matplotlib.pyplot as plt
+import pygame, numpy as np, matplotlib.pyplot as plt
 
-screen = pygame.display.set_mode((600,400), 0, 8)
+screen = pygame.display.set_mode((100,100), 0, 8)
 BKG = (0, 0, 0)
 screen.fill(BKG)  # white screen
 draw_on = False
 last_pos = (0, 0)
 color = (255, 255, 255)
-radius = 2
+radius = 1
 
 def roundline(srf, color, start, end, radius=1):
     dx = end[0]-start[0]
@@ -45,14 +45,38 @@ print(mapArr[0].size)
 start = (0, 0)
 goal = (0, 0)
 
+# CUSTOMIZE START/GOAL
+pos_arr = [start, goal]
+i = 0
+try:
+    while True:
+        while i < len(pos_arr):
+            e = pygame.event.wait()
+            if e.type == pygame.QUIT or i >= len(pos_arr):
+                raise StopIteration
+            if e.type == pygame.MOUSEBUTTONUP:
+                x, y = pygame.mouse.get_pos()
+                pos_arr[i] = (x, y)
+                if mapArr[x][y] == 1.:
+                    print("Invalid point chosen.")
+                    pass
+                else:
+                    print("Valid point chosen.")
+                    i += 1
+                    if i >= len(pos_arr):
+                        raise StopIteration
+            if e.type == pygame.MOUSEMOTION:
+                pass
+            pygame.display.flip()
+
+except StopIteration:
+    pass
+
 # MAP OUTPUT
 fig, ax = plt.subplots(figsize=(12,12))
-
 ax.imshow(mapArr, cmap=plt.cm.tab20b)
-
-ax.scatter(start[1],start[0], marker = "*", color = "yellow", s = 200)
-
-ax.scatter(goal[1],goal[0], marker = "*", color = "red", s = 200)
-
+ax.scatter(pos_arr[0][1], pos_arr[0][0], marker = "*", color = "yellow", s = 200)
+ax.scatter(pos_arr[1][1], pos_arr[1][0], marker = "*", color = "red", s = 200)
 plt.show()
+
 pygame.quit()
